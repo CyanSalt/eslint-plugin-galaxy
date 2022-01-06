7b2dfffe2765afcbfd86a52f8fbac058be7c00a6
CyanSalt <c.salt@hotmail.com>
2022-01-06 21:52:30 +0800

    fix: import path fixer

--- src/rules/import-extensions.ts
+++ src/rules/import-extensions.ts
@@ -126,7 +126,7 @@ export default createRule({
             fix(fixer) {
               let fixedPath = importPath + extension
               if (resolve(fixedPath, context) !== resolvedPath) {
-                fixedPath = path.join(importPath, 'index' + extension)
+                fixedPath = path.format({ dir: importPath, name: 'index', ext: extension })
               }
               const query = importPathWithQueryString.slice(importPath.length)
               return fixer.replaceText(source, `'${fixedPath}${query}'`)
